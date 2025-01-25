928246ba8a4ce2a111b48b3e811525e2ae27af9c
ajaysharma2053 <ajaysharma2053@gmail.com>
2025-01-24 23:14:03 -0500

    Deploying new version of the app

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("Hi, this is Ajay. This your new version of App. You've hit " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
